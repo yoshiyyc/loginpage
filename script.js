3df1c6c8961ec884fe5c89c0658dc5fdb1338eaf
Yoshi Y <95995009+yoshiyyc@users.noreply.github.com>
2023-05-22 01:11:52 +0800

    Testing

--- script.js
+++ script.js
@@ -139,6 +139,7 @@ async function setLocale(newLocale) {
 // Retrieve translations JSON object for the given locale over the network
 async function fetchTranslationsFor(newLocale) {
   const response = await fetch(`https://yoshiyyc.github.io/loginpage/lang/${newLocale.toLowerCase()}.json`);
+  console.log(`https://yoshiyyc.github.io/loginpage/lang/${newLocale.toLowerCase()}.json`);
   return await response.text();
 }
 
